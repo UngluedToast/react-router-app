7d6ab313906f0d3f8b3809b79117f5769d069bc5
Andrew Josey <JoganLosey@gmail.com>
2019-05-01 14:42:15 -0400

    added links and routes, more rats mate

--- src/App.js
+++ src/App.js
@@ -5,34 +5,88 @@ import About from './about';
 import Rats from './rat';
 import PageNotFound from './PageNotFound'
 import Header from './Header'
+import Ratty from './Rats'
 
 
 import {
   Link,
   Route,
-  Switch
+  Switch,
+  withRouter
 } from 'react-router-dom';
 
-function App() {
+// function AboutButton({history}) {
+//   return(
+//     <button onClick = {() => {
+//       history.push('./about');
+//     }} >Go to the About page</button>
+//   )
+
+// }
+
+const SuperAboutButton = withRouter(({history}) => {
   return (
-    <div className="App">
+    <button onClick = {() => {
+      history.push('/about');
+  }} >Go to the About page</button>
+  )
+})
 
-    <Header/>
-    <br />
-      <Link to="/">Home</Link>
-    <br />
-      <Link to="/Rats">Rats</Link>
-    <br />
-      <Link to="/About">About</Link>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Rats: [
+        'Ikit',
+        'Queek',
+        'Skrolk',
+        'Tretch'
+      ]
+    }
+  }
 
-      <Switch>
-        <Route  exact path="/" component={Home}/>
-        <Route path="/Rats" component={Rats}/>
-        <Route path="/About" component={About}/>
-        <Route component={PageNotFound}/>
-      </Switch>
-    </div>
-  );
+
+  render() {
+    return (
+      <div className="App">
+
+      <Header/>
+      <br />
+        <Link to="/">Home</Link>
+      <br />
+        <Link to="/Rats">Rats</Link>
+      <br />
+        <Link to="/About">About</Link>
+        <SuperAboutButton/>
+
+        <Switch>
+          <Route  exact path="/" component={Home}/>
+
+
+
+          {/* <Route path="/Rats" component={Rats}/> */}
+          <Route path="/Rats"
+            render={(props) => (
+              <Rats
+              {...props}
+              rats={this.state.cats}
+              /> 
+            )}
+          />
+
+
+
+          <Route path="/About" component={About}/>
+
+
+
+          <Route component={PageNotFound}/>
+        </Switch>
+        <Route path="/Rats/:rat" component={Ratty}/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/rat.js
+++ src/rat.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import history from 'react'
+
+import { Link } from 'react-router-dom';
+
+const arrayOfRats= [
+    'Ikit',
+    'Queek',
+    'Skrolk',
+    'Tretch'
+];
+
+function Rats({match, history}) {
 
 
-function Rats(props) {
+
+    const ratLinks = arrayOfRats.map(ratName => (
+        <li>
+            <Link to={`${match.path}/${ratName}`}>{ratName}</Link>
+        </li>
+    ));
+
     return (
         <div>
             <h1>🐀</h1>
             <h1>🐀</h1>
             <h1>🐀</h1>
             <h1>🐀</h1>
-            <h1>🐀</h1>
-            <h1>🐀</h1>
-            <h1>🐀</h1>
-            <h1>🐀</h1>
-            <h1>🐀</h1>
-            <h1>🐀</h1>
+            <ul>
+                {ratLinks}
+            </ul>
+            <button onClick={() => {
+                history.push('/');
+            }}>Go Home</button>
             <h2> DAMN THATS A LOTTA RATS </h2>
         </div>
     );
