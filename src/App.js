d9a4bcb9d0bc752fe99882e44bc2917d648509e9
Andrew Josey <JoganLosey@gmail.com>
2019-05-01 11:10:09 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './home';
+import About from './about';
+import Rats from './rat';
+import PageNotFound from './PageNotFound'
+import Header from './Header'
+
+
+import {
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Header/>
+    <br />
+      <Link to="/">Home</Link>
+    <br />
+      <Link to="/Rats">Rats</Link>
+    <br />
+      <Link to="/About">About</Link>
+
+      <Switch>
+        <Route  exact path="/" component={Home}/>
+        <Route path="/Rats" component={Rats}/>
+        <Route path="/About" component={About}/>
+        <Route component={PageNotFound}/>
+      </Switch>
     </div>
   );
 }
